c22f0cd9338261f729ab50bcfe8b49a4d42e43af
Tristan PINTO <tristan.pin81@gmail.com>
2023-12-14 11:04:28 +0100

    Resolves 4

--- app/src/main/java/helloandroid/ut3/com/as_tp2/MainActivity.java
+++ app/src/main/java/helloandroid/ut3/com/as_tp2/MainActivity.java
@@ -3,12 +3,19 @@ package helloandroid.ut3.com.as_tp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Load toto layout
         setContentView(R.layout.toto);
+
+        // Get the TextView1
+        TextView tv = (TextView) findViewById(R.id.textView1);
+        tv.setText("Bienvenue chez vous");
     }
 }
