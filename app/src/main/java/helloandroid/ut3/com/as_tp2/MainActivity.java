0c5f51f9ae4f4a811d4bb9bcc546924eadc0ee91
Tristan PINTO <tristan.pin81@gmail.com>
2023-12-14 11:20:34 +0100

    Resolves 5

--- app/src/main/java/helloandroid/ut3/com/as_tp2/MainActivity.java
+++ app/src/main/java/helloandroid/ut3/com/as_tp2/MainActivity.java
@@ -3,9 +3,14 @@ package helloandroid.ut3.com.as_tp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int counter = 0;
+    private TextView tv = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +20,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.toto);
 
         // Get the TextView1
-        TextView tv = (TextView) findViewById(R.id.textView1);
+        tv = (TextView) findViewById(R.id.textView1);
         tv.setText("Bienvenue chez vous");
+
+        // Button personalization (JAVA way)
+        /*
+        Button b = (Button) findViewById(R.id.button1);
+        b.setOnClickListener(monEcouteur);
+        */
+    }
+
+    // Button callback (JAVA way)
+    /*
+    private View.OnClickListener monEcouteur = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Log.d("button1", "pressed!");
+        }
+    };
+    */
+
+    public void mehtodeBouton(View view) {
+        counter++;
+        tv.setText("" + counter);
     }
 }
