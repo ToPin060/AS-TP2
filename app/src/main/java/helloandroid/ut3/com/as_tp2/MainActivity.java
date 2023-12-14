9b5a6c4b9bb58ce30a85aa6292fcefc03621ef52
Tristan PINTO <tristan.pin81@gmail.com>
2023-12-14 10:43:03 +0100

    Android studio initial empty project

---
+++ app/src/main/java/helloandroid/ut3/com/as_tp2/MainActivity.java
@@ -0,0 +1,14 @@
+package helloandroid.ut3.com.as_tp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
